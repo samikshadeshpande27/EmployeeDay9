fea545cd109faf8afbcb5c3096d4e5c34237984b
samiksha <samikshadeshpande27@gmail.com>
2020-05-18 01:14:48 -0700

    UC1 IS ADDED

--- Day9.sh
+++ Day9.sh
@@ -1,1 +1,14 @@
+#!/bin/bash -x
 echo "Welcome to Employeewage"
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ];
+then
+   echo "EMPLOYEE IS PRESENT";
+else
+    echo "EMPLOYEE IS ABSENT";
+fi
+
+
+
+
