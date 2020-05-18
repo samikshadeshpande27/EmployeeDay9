f9878fabd3aa45b9e02fcd3383f9351941144538
samiksha <samikshadeshpande27@gmail.com>
2020-05-18 02:58:59 -0700

    UC4 is ADDED

--- Day9.sh
+++ Day9.sh
@@ -1,18 +1,22 @@
 #!/bin/bash -x
 isPartTime=1;
-isFullTime=2;
+isFulTime=2;
 empRatePerHr=20;
-randomCheck=$((RANDOM%3));
-if [ $isFullTime -eq $randomCheck ];
-then
-    empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then
-     empHrs=4;
-else
-     empHrs=0;
-fi
+empCheck=$((RANDOM%3));
+case $empCheck in
+     $isFullTime)
+          empHrs=8
+             ;;
+     $isPartTime)
+        empHrs=4
+          ;;
+         *)
+        empHrs=0
+         ;;
+esac
 salary=$(($empHrs*$empRatePerHr));
+
+
 
 
 
