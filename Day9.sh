1eca665c8161e314373418597fbd0664397ab7ba
samiksha <samikshadeshpande27@gmail.com>
2020-05-18 03:11:56 -0700

    UC7 is ADDED

--- Day9.sh
+++ Day9.sh
@@ -1,32 +1,39 @@
 #!/bin/bash -x
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
-MAX_HRS_IN_MONTH=10;
+MAX_HRS_IN_MONTH=4;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
 totalEmpHr=0;
 totalWorkingDays=0;
 
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
+function getWorkingHours() {
+     case $1 in
+           $IS_FULL_TIME)
+             workHours=8
+              ;;
+          $IS_PART_TIME)
+           workHours=4
+              ;;
+           *)
+           workHours=0
+             ;;
+      esac
+        echo $workHours
+}
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH &&
         $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 ((totalWorkingDays++))
-empCheck=$((RANDOM%3));
-     case $empCheck in
-           $IS_FULL_TIME)
-             empHrs=8
-              ;;
-          $IS_PART_TIME)
-           empHrs=4
-              ;;
-           *)
-           empHrs=0
-             ;;
-      esac
-     totalEmpHrs=$[($totalEmpHrs+$empHrs)]
+
+workHours="$( getWorkingHours $((RANDOM%3)) )"
+totalWorkHours=$(($totalWorkHours*$workHours))
 done
-    totalSalary=$[($totalEmpHrs*$EMP_RATE_PER_HR)];
+    totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR));
+
+
+
 
 
 
